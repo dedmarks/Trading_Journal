195db0a13f8966f514e51a9231447a6546a5b6c6
dedmarks <dedmark99@gmail.com>
2022-07-17 19:47:02 +0200

    9th

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import {getFirsestore, collection, getDoc, getDocs, doc} from 'firebase/firestore'
+import {getDocs} from 'firebase/firestore'
 
 
 const firebaseConfig = {
